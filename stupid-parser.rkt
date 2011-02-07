d0efc961149b5e1403839dbd7f510115c5749533
David Percy <dpercy@cs.brown.edu>
2011-02-07 18:26:45 -0500

    great success\! a working stupid parser

--- stupid-parser.rkt
+++ stupid-parser.rkt
@@ -2,18 +2,42 @@
 (require parser-tools/yacc)
 (require parser-tools/lex)
 
-(define-empty-tokens ops (+ - * /))
+(define-empty-tokens ops (plus minus times divide))
 (define-empty-tokens parens (open close))
 (define-tokens nums (num))
+(define-empty-tokens ending (EOF))
+
+
+(define lex
+  (lexer
+   [whitespace (lex input-port)]
+   [(repetition 1 +inf.0 (char-range #\0 #\9)) (token-num (string->number lexeme))]
+   ["+" (token-plus)]
+   [(eof) (token-EOF)]
+   ))
 
 (define parse
   (parser
    
-   (tokens ops parens nums)
+   (tokens ops parens nums ending)
    
    (grammar
-    (numexp ((num))))
+    (expr [(numexp) $1]
+          [(sumexp) $1])
+    (sumexp [(num plus num) (list $1 $3)])
+    (numexp [(num) $1])
+    
+    
+    )
    
-   (error (λ () (error "OH NO")))
+   
+   (start expr)
+   (end EOF)
+   (error (λ (valid? token value) 
+            (error 
+             (format "parse error at ~a: with value:~a was token valid?:~a"
+                     token 
+                     value 
+                     valid?))))
    
    ))
