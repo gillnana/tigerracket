451918ef31335b20111c0c7be435721742bd8d77
Roan A. LaPlante <rlaplant@cs.brown.edu>
2011-02-20 16:56:28 -0500

    added type, variable, and untested function declarations to type checker.  these things now propagate in the type and variable environments.  complex types are still unchecked

--- typecheck.rkt
+++ typecheck.rkt
@@ -25,12 +25,20 @@
   (cond 
     [(equal? type-symbol 'int) (t-int)]
     [(equal? type-symbol 'string) (t-string)]
+    ;[(type-id? type-symbol) (type-lookup (type-id-name type-symbol) type-env)]
     [else (or (ormap (lambda (binding) (if (equal? (type-binding-id binding) type-symbol)
                                (type-binding-ty binding)
                                false))
                      type-env)
               (error (format "unbound type ~a" type-symbol)))]))
-    
+
+(define (var-lookup var-symbol var-env) 
+  (or (ormap (lambda (binding) (if (equal? (var-binding-id binding) var-symbol)
+                                   (var-binding-ty binding)
+                                   false))
+             var-env)
+      (error (format "unbound identifier ~a" var-symbol))))
+
 
 (define (type-of expr)
   (type-of-env expr empty empty))
@@ -43,22 +51,22 @@
     
     [(array-creation (type-id type) size initval)
      (cond
-       [(not (equal? (type-of-env size te ve) (t-int))) (error "type of array size not int")]
-       [(not (equal? type (type-of-env initval te ve))) (error "type of array not same as initial value")]
+       [(not (equal? (type-of-env size te ve) (t-int))) (error "type error: type of array size not int")]
+       [(not (equal? type (type-of-env initval te ve))) (error "type error: type of array not same as initial value")]
        [else (t-array (type-lookup type te))])]
     
     [(binary-op (op sym) arg1 arg2)
      (cond [(symbol=? sym '=) (if (equal? (type-of-env arg1 te ve) (type-of-env arg2 te ve))
                                   (t-int)
-                                  (error "arguments for equality comparison must be of same type"))]
+                                  (error "type error: arguments for equality comparison must be of same type"))]
            [(and (equal? (type-of-env arg1 te ve) (t-int))
                       (equal? (type-of-env arg2 te ve) (t-int)))
             (t-int)]
-           [else (error (format "args for operator ~a must be integers" sym))])]
+           [else (error (format "type error: args for operator ~a must be integers" sym))])]
     
     [(unary-op (op '-) arg1)
      (if (not (equal? (type-of-env arg1 te ve) (t-int)))
-         (error "arg to unary minus must be int")
+         (error "type error: arg to unary minus must be int")
          (t-int))]
     
     [(sequence (list)) (t-void)]
@@ -66,54 +74,69 @@
      (type-of-env b te ve)]
     
     [(if-statement c t (list))
-     (cond [(not (equal? (type-of-env c te ve) (t-int))) (error "condition of if statement must have boolean/int value")]
-           [(not (equal? (type-of-env t te ve) (t-void))) (error "then branch of an if statement must have no value")]
+     (cond [(not (equal? (type-of-env c te ve) (t-int))) (error "type error: condition of if statement must have boolean/int value")]
+           [(not (equal? (type-of-env t te ve) (t-void))) (error "type error: then branch of an if statement must have no value")]
            [else (t-void)])]
     [(if-statement c t e)
      (let ([type-of-t (type-of-env t te ve)]) 
-       (cond [(not (equal? (type-of-env c te ve) (t-int))) (error "condition of if statement must have boolean/int value")]
-             [(not (equal? type-of-t (type-of-env e te ve))) (error "then and else branches of if statement must have same type")]
+       (cond [(not (equal? (type-of-env c te ve) (t-int))) (error "type error: condition of if statement must have boolean/int value")]
+             [(not (equal? type-of-t (type-of-env e te ve))) (error "type error: then and else branches of if statement must have same type")]
              [else type-of-t]))]
     [(while-statement c body)
      (if (equal? (type-of-env c te ve) (t-int))
          (type-of-env body te ve)
-         (error "while statement condition must have boolean/int value"))]
+         (error "type error: while statement condition must have boolean/int value"))]
     [(for-statement var start end body)
      (if (and ;l-value-of var = int
               (equal? (type-of-env start te ve) (t-int))
               (equal? (type-of-env end te ve) (t-int)))
          (type-of-env body te ve)
-         (error "for statement must increment an int from start to end int values"))]
+         (error "type error: for statement must increment an int from start to end int values"))]
+    [(let-statement decs (list))
+     (t-void)] ; TODO: handle void expressions more intelligently, by implementing the function defined below. since it needs access
+               ; to the environments, where should this function be located?
     [(let-statement decs (list a ... b))
-     ; TODO: do this for each environments
-     (foldl (lambda (binding env)
-              (match binding
-                [(tydec type-id ty) 
-                 ; TODO: add the binding to the env
-                 (error 'TODO)
-                 ]
-                [(vardec id id-type val) 
-                 (error 'TODO)
-                 ; TODO: add the binding to the env
-                 ]
-                [(fundec id tyfields type-id body) 
-                 (error 'TODO)
-                 ; TODO: add the binding to the env
-                 ]
-              )
-            the-environment
-            decs
-            )
-     
-     
-     
-     (type-of-env b )]
+     (local [(define (accumulate-declarations binding env)
+                (match binding
+                  [(tydec type-id ty) 
+                   (cons (type-binding type-id ty) env)]
+                  [(vardec id id-type val)
+                   (if (not id-type)
+                       (cons (var-binding id (infer-primitive val ve)) env)
+                       (let [(declared-type (type-lookup id-type te))
+                             (expression-type (infer-primitive val ve))]
+                         (if (equal? declared-type expression-type)
+                             (cons (var-binding id id-type) env)
+                             (error (format "type error: type mismatch, found ~a; expected ~a"
+                                            declared-type expression-type)))))]
+                  [(fundec id tyfields type-id body) 
+                   (if (not type-id)
+                       env ; function definitions never add any new types and only need to be checked for mismatches, so we can ignore
+                           ; this case, right?
+                       (let [(declared-type (type-lookup type-id te))
+                             (expression-type (infer-primitive body ve))]
+                         (if (equal? declared-type expression-type)
+                             env
+                             (error (format "type error: type mismatch, found ~a; expected ~a"
+                                            declared-type expression-type)))))]))]
+                     ;TODO: test function definitions
+       (type-of-env b
+                    (foldl accumulate-declarations
+                           te ; the environment
+                           decs)
+                    ve))]
     
               
     ;[(record-creation (type-id type) fields)
     ;(if (check-record-fields fields (type-lookup type te))
     ))
 
+(define (infer-primitive expr ve)
+  (cond [(int-literal? expr) (t-int)]
+        [(string-literal? expr) (t-string)]
+        [(nil? expr) (error "type error: nil expressions must have a declared type")]
+        [(id? expr) (var-lookup (type-id-name expr) ve)]
+        [else (error (format "type error: failed to infer a basic primitive type from initial value ~a.  polymorphic types are not allowed" expr))]))
 
 
 ;;TESTS
@@ -137,9 +160,24 @@
               (t-int))
 (check-expect (type-of (parse-string "if 4 then 7 else 6"))
               (t-int))
-(check-error (type-of (parse-string "if 4 then \"hamburger\" else 12")) "then and else branches of if statement must have same type")
+(check-error (type-of (parse-string "if 4 then \"hamburger\" else 12")) "type error: then and else branches of if statement must have same type")
 (check-expect (type-of (parse-string "while 1 do (27; \"spain\")"))
               (t-string))
 
+(check-expect (type-of (parse-string "let type a = int in 4 end"))
+              (t-int))
+(check-expect (type-of (parse-string "let type f = int type g = int in 5 end"))
+              (t-int))
+(check-expect (type-of (parse-string "let var y := 4 in 29 end"))
+              (t-int))
+(check-expect (type-of (parse-string "let var u : int := 13 in 100 end"))
+              (t-int))
+
+(check-error (type-of (parse-string "let var w : string := 22 in 1 end")) "type error: type mismatch, found #(struct:t-string); expected #(struct:t-int)")
+(check-error (type-of (parse-string "let type a = int in let var x : a := \"green\" in 37 end end")) "type error: type mismatch, found #(struct:type-id int); expected #(struct:t-string)")
+;(check-error (type-of (parse-string "let var z := nil in end")) "") ; TODO: handle nil values more intelligently
+
+;(check-expect (type-of (parse-string "let type a = int var x : a := 2 in 154 end")) (t-int))
+;(check-expect (type-of (parse-string "let type a = int type b = a in let var nobbish : b := 48 in 23 end end")) (t-int)) ; TODO: let = letrec*
 
 (test)
