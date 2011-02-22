3681e3c7efa978dd85ae9864a71b78d92e8a488c
Roan A. LaPlante <rlaplant@cs.brown.edu>
2011-02-22 15:37:41 -0500

    added array access, creation, and contracts to type checker

--- typecheck.rkt
+++ typecheck.rkt
@@ -21,6 +21,7 @@
 (struct t-record (fields) #:transparent)
 (struct field (name ty) #:transparent)
 
+; type-lookup symbol listof-type-binding -> t-type
 (define (type-lookup type-symbol type-env) 
   (cond 
     [(equal? type-symbol 'int) (t-int)]
@@ -33,21 +34,27 @@
                      type-env)
               (error (format "unbound type ~a" type-symbol)))]))
 
+; var-lookup symbol listof-var-binding -> t-type
 (define (var-lookup var-symbol var-env)
   ;(print var-env)
   (or (ormap (lambda (binding) (if (equal? (var-binding-id binding) var-symbol)
                                    (var-binding-ty binding)
                                    false))
              var-env)
-      (error (format "unbound identifier ~a" var-symbol))))
+      (error (format "unbound identifier ~a in environment ~a" var-symbol var-env))))
 
 ; takes the type of an identifier or function argument, and the type of a thing you want to put in it
 ; tells you if that's ok
+; assignable-to? t-type t-type -> boolean
 (define (assignable-to? variable value)
-  (or (equal? value (t-nil))
-      (equal? value variable)))
+  ;(print variable)
+  ;(print value)
+  (when (symbol? variable) (error "internal error: assignable-to received a symbol"))
+  (and (not (t-void? value))
+       (or (equal? value (t-nil))
+           (equal? value variable))))
 
-
+; type-of expr -> t-type
 (define (type-of expr)
   (type-of-env expr empty empty))
 
@@ -60,10 +67,20 @@
     [(id a) (var-lookup a ve)]
     
     [(array-creation (type-id type) size initval)
+     ;(print type)
+     ;(print initval)
      (cond
        [(not (t-int? (type-of-env size te ve))) (error "type error: type of array size not int")]
-       [(not (equal? type (type-of-env initval te ve))) (error "type error: type of array not same as initial value")]
+       [(not (assignable-to? (type-lookup type te) (type-of-env initval te ve))) (error "type error: type of array not same as initial value")]
        [else (t-array (type-lookup type te))])]
+    [(array-access (id name) index)
+     (if (t-int? (type-of-env index te ve))
+         (t-array-elem-type (var-lookup name ve))
+         (error "type error: attempted to access array ~a with non-integer index"))]
+          
+          
+    ;[(record-creation (type-id type) fields)
+    ;(if (check-record-fields fields (type-lookup type te))
     
     [(binary-op (op sym) arg1 arg2)
      (cond [(symbol=? sym '=) (if (equal? (type-of-env arg1 te ve) (type-of-env arg2 te ve))
@@ -124,7 +141,7 @@
                   (let [(expression-type (type-of-env val te ve))]
                       (if (not id-type)
                         (if (equal? (t-nil) expression-type)
-                            (error (format "variable ~a has value nil but no type" id))
+                            (error (format "type error: variable ~a has value nil but no type" id))
                             (cons (var-binding id expression-type) v-env))
                         (let [(declared-type (type-lookup id-type te))]
                           (if (assignable-to? declared-type expression-type)
@@ -148,7 +165,7 @@
                               (error (format "type error: type mismatch, found ~a; expected ~a"
                                              declared-type body-type))))))]
                  [(tydec a b) v-env]
-                 [(while-statement cond body) (error "while statement found in odd place")] ; just in case
+                 [(while-statement cond body) (error "type error: while statement found in odd place")] ; just in case
                  ))]
            
        ;TODO: test function definitions
@@ -161,8 +178,6 @@
                            decs)))]
      ;end let statement
               
-    ;[(record-creation (type-id type) fields)
-    ;(if (check-record-fields fields (type-lookup type te))
     ))
 
 ;(define (infer-primitive expr ve)
@@ -207,6 +222,22 @@
 (check-expect (type-of (parse-string "let var u : int := 13 in 100 end"))
               (t-int))
 
+; array creation/access tests
+(check-error (type-of (parse-string "a[\"zoomba\"]")) "type error: attempted to access array ~a with non-integer index")
+(check-error (type-of (parse-string "let var x := int[4] of \"pizza\" in end")) "type error: type of array not same as initial value")
+(check-expect (type-of (parse-string "let var y := int[26] of nil in y end")) (t-array (t-int)))
+
+(check-error (type-of (parse-string "a[4]")) "unbound identifier a in environment ()")
+
+(check-expect (type-of (parse-string "let var ab := int[10] of 5 in ab[24] end")) (t-int))
+;(check-error (type-of (parse-string "let var z := int[10] of 5 in z[\"pizza\"] end")) "")
+;(check-error (type-of (parse-string "let var aa := int[10] of 5 in aa[nil] end")) "")
+
+
+(check-expect (type-of (parse-string "(34; 27)")) (t-int))
+
+; let-statement tests
+
 (check-error (type-of (parse-string "let var w : string := 22 in 1 end")) "type error: type mismatch, found #(struct:t-string); expected #(struct:t-int)")
 (check-error (type-of (parse-string "let type a = int in let var x : a := \"green\" in 37 end end")) "type error: type mismatch, found #(struct:type-id int); expected #(struct:t-string)")
 
@@ -214,8 +245,8 @@
               (t-int))
 (check-expect (type-of (parse-string "let var n := 5*3+2-12*66-304440403 in \"waaaa\" end"))
               (t-string))
-(check-error (type-of (parse-string "let var z := nil in end")) "variable z has value nil but no type") ; TODO: handle nil values more intelligently
-(check-error (type-of (parse-string "let var z := nil in zz end")) "variable z has value nil but no type") ; TODO: handle nil values more intelligently
+(check-error (type-of (parse-string "let var z := nil in end")) "type error: variable z has value nil but no type")
+(check-error (type-of (parse-string "let var z := nil in zz end")) "type error: variable z has value nil but no type")
 (check-expect (type-of (parse-string "let var zz : int := nil in end")) (t-void))
 ;(check-expect (type-of (parse-string "let type a = int var x : a := 2 in 154 end")) (t-int)) ; TODO: let*
 ;(check-expect (type-of (parse-string "let type a = int type b = a in let var nobbish : b := 48 in 23 end end")) (t-int)) ; TODO: let = letrec*
