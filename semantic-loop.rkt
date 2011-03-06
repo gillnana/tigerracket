ae160f71d61b54fe2d2ca241197b4c9ca6771d80
Roan A. LaPlante <rlaplant@cs.brown.edu>
2011-03-06 17:51:18 -0500

    fixed iterator shadowing. added test cases

--- semantic-loop.rkt
+++ semantic-loop.rkt
@@ -4,7 +4,7 @@
 (require test-engine/racket-tests)
 
 ;iterator? is true iff the local variable is a for loop indexing variable
-(struct iterator-binding (id iterator?))
+(struct iterator-binding (id iterator?) #:transparent)
 
 ; verify-loop-sematics : ast-node bool listof-iterator-binding -> void
 ; throws an error if
@@ -16,7 +16,7 @@
 (define (vls node in-loop? env)
   (match node
     [(for-statement var start end body) 
-     (vls body true (cons (iterator-binding var true) env))]
+     (vls body true (cons (iterator-binding var #t) env))]
     
     [(while-statement cond body)
      (begin
@@ -24,12 +24,19 @@
        (vls body true env))]
     
     [(assignment (id name) expr)
-     (begin (map (match-lambda
-                   [(iterator-binding iter-id #t) 
-                   (when (equal? iter-id name)   
-                     (error (format "semantic error: illegal assignment to iterator ~a" iter-id)))])
-                 env)
-            (vls expr in-loop? env))]
+     (local
+       [(define (assn-helper name env)
+          (match env
+            [(list) (void)]
+            [(cons (iterator-binding iter-id bool) rest)
+             (if (equal? iter-id name)
+                 (if bool 
+                     (error (format "semantic error: illegal assignment to iterator ~a" iter-id))
+                     (void))
+                 (assn-helper name rest))]))]
+       (assn-helper name env)
+       (vls expr in-loop? env))]
+                  
     [(assignment l-expr r-expr)
      (begin
      (vls l-expr in-loop? env)
@@ -39,26 +46,27 @@
     
     [(let-vars bindings body)
      (vls body in-loop? 
-                            (foldl (match-lambda* 
-                                     [(list (vardec id t-id val) env-accum)
-                                      (begin
-                                        (vls val in-loop? env-accum)
-                                        (cons (iterator-binding id false) env-accum))])
-                                   env
-                                   bindings))]
+          (foldl (match-lambda* 
+                   [(list (vardec id t-id val) env-accum)
+                    (begin
+                      (vls val in-loop? env-accum)
+                      (cons (iterator-binding id #f) env-accum)
+                      )])
+                 env
+                 bindings))]
     
     [(let-funs bindings body)
      (map (match-lambda
             [(fundec id tyfields type-id body)
-             (vls body in-loop?
-                                    (foldl (match-lambda*
-                                             [(list (tyfield name type-id) env-accum)
-                                              (cons (iterator-binding name #f) env-accum)])
-                                           env
-                                           tyfields))
+             (vls body #f
+                  (foldl (match-lambda*
+                           [(list (tyfield name type-id) env-accum)
+                            (cons (iterator-binding name #f) env-accum)])
+                         env
+                         tyfields))
              ])
-            bindings)
-            ]
+          bindings)
+     ]
     
     [(int-literal val) (void)]
     [(string-literal val) (void)]
@@ -97,8 +105,6 @@
     
     [(expseq seq)
      (map (λ (exp) (vls exp in-loop? env)) seq)]
-    [(sequence expseq)
-     (vls expseq in-loop? env)]
     
     [(break) (unless in-loop? (error "semantic error: found a break statement not inside a loop"))]
     )
@@ -112,5 +118,11 @@
 (check-error (verify-loop-semantics (parse-string "break")) "semantic error: found a break statement not inside a loop")
 (check-error (verify-loop-semantics (parse-string "if 45 then break else break")) "semantic error: found a break statement not inside a loop")
 (check-expect (verify-loop-semantics (parse-string "for i := 1 to 10 do (while i < 50 do if i=30 then break else (); break)")) (void))
+(check-error (verify-loop-semantics (parse-string "for i := 1 to 10 do i := 5")) "semantic error: illegal assignment to iterator i")
+(check-expect (verify-loop-semantics (parse-string "for i := 1 to 10 do let var i:int := 10 in i := 5 end")) (void))
+(check-error (verify-loop-semantics (parse-string "for i := 1 to 10 do let function f(x:int,y:int):int = (break;7) in f(3,4) end")) "semantic error: found a break statement not inside a loop")
+(check-error (verify-loop-semantics (parse-string "for i := 1 to 10 do let function f(x:int,y:int):int = (break;7) in while (1) do f(3,4) end")) "semantic error: found a break statement not inside a loop")
+;(check-error (verify-loop-semantics (parse-string "for i := 1 to 10 do i := 5")) "semantic error: illegal assignment to iterator i")
+;(check-error (verify-loop-semantics (parse-string "for i := 1 to 10 do i := 5")) "semantic error: illegal assignment to iterator i")
  
 (test)
