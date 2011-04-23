15152452cd4ce6f677d908fd09cf025f8d760ff3
David Percy <dpercy@cs.brown.edu>
2011-04-23 19:26:58 -0400

    better asm script outputs human readable assembly

--- mips_fun/asm.sh
+++ mips_fun/asm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+mips-linux-gcc -S -O0 -mips1 -mno-abicalls -nostdlib -o libtiger.s libtiger.c 
+ 
 mips-linux-gcc -c -O0 -mips1 -mno-abicalls -nostdlib -o libtiger.o libtiger.c  
 mips-linux-as -O0 -mips1 -o "$1.o" "$1.s"
 mips-linux-ld -o "$1" "$1.o" libtiger.o /course/cs195r/lib/mips-rt/mips-rt.a  
