04dcf80ed98f4d90894cefc4abf5525828fc8516
Roan A. LaPlante <rlaplant@cs.brown.edu>
2011-04-24 02:00:21 -0400

    fixed the dumb bullshit bug

--- mips_fun/wtf.s
+++ mips_fun/wtf.s
@@ -8,25 +8,27 @@ main:
 	sub $sp, $sp, 4
 	sw $ra, 4($sp)
 
-	li $a0, 35
+   li $a0, 35
 	li $a1, 33  # the letter 'N'
 #	jal alloc_block
+   sub $sp, $sp, 16
 	jal alloc_array
+   add $sp, $sp, 16
 	move $s0, $v0
 	
 
-#	add $t0, $s0, 0
-#	lw $a0, ($t0)
-#	jal lt_print_int
+	add $t0, $s0, 0
+	lw $a0, ($t0)
+	jal lt_print_int
 	
-#	add $t0, $s0, 4
-#	lw $a0, ($t0)
-#	jal lt_print_int
+	add $t0, $s0, 4
+	lw $a0, ($t0)
+	jal lt_print_int
 	
 	
 	# $s0 is pointer to block of 7 letter 'N'
-#	move $a0, $s0
-#	jal lt_print
+	move $a0, $s0
+	jal lt_print
 
 
 	
